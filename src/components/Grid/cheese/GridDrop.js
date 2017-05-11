72f6e5a299cb3cb14a13d81be014b358fddf9c9a
Your Name <myname@name.me>
2017-05-11 21:53:03 +0300

    'add drop layer progress'

---
+++ src/components/Grid/cheese/GridDrop.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Square from './Square';
+// import Knight from './Knight';
+
+export default class Board extends Component {
+  renderSquare(i) {
+    const x = i % 10;
+    const y = Math.floor(i / 10);
+    const black = (x + y) % 2 === 1;
+
+    // const [knightX, knightY] = this.props.knightPosition={[7, 4]};
+    // const piece = (x === 7 && y === 4) ?
+    //   <Knight /> :
+    //   null;
+
+    return (
+      <div key={i}
+           style={{ width: '10%', height: '10%' }}>
+        <Square black={black}>
+        </Square>
+      </div>
+    );
+  }
+
+  render() {
+    const squares = [];
+    for (let i = 0; i < 100; i++) {
+      squares.push(this.renderSquare(i));
+    }
+
+    return (
+      <div style={{
+        width: '300px',
+        height: '253px',
+        display: 'flex',
+        flexWrap: 'wrap',
+        position: 'absolute',
+        zIndex: '999',
+        opacity: '0.1',
+        top: '144px',
+        right: '353px',
+      }}>
+        {squares}
+      </div>
+    );
+  }
+}
+
+Board.propTypes = {
+  // knightPosition: PropTypes.arrayOf(
+  //   PropTypes.number.isRequired
+  // ).isRequired
+};
