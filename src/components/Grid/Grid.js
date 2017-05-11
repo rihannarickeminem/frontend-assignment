eb7fc6b3431004b4b9d8b43ee0042d596ec25c87
Your Name <myname@name.me>
2017-05-11 19:50:06 +0300

    'track progress'

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,9 +1,119 @@
-import React from 'react'
-
+import React, { Component } from 'react'
 import classes from './Grid.scss'
 import * as d3 from "d3";
-const Xgrid = require('react-d3-core').Xgrid;
-const Ygrid = require('react-d3-core').Ygrid;
+import Faux from 'react-faux-dom'
+
+class LineChart extends Component {
+	constructor(props) {
+		super(props);
+    this.state = {
+      width: this.props.width,
+      height: this.props.height,
+    };
+	}
+	updateDimensions() {
+		// this.setState({width: $(window).width(), height: $(window).height()});
+	}
+
+	componentWillMount() {
+		this.updateDimensions();
+	}
+
+	componentDidMount() {
+		window.addEventListener("resize", this.updateDimensions);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.updateDimensions);
+	}
+	getGridData() {
+		let { width: widthMain,
+			height: heightMain } = this.state;
+		let data = [];
+		let xpos = 1;
+		let ypos = 1;
+		let width = (widthMain-10)/10;
+		let height = (heightMain-10)/10;
+
+		for (var row = 0; row < 10; row++) {
+			data.push( [] );
+			for (var column = 0; column < 10; column++) {
+				data[row].push({
+					x: xpos,
+					y: ypos,
+					width: width,
+					height: height
+				})
+				xpos += width;
+			}
+			xpos = 1;
+			ypos += height; 
+		}
+		return data;
+	}
+
+	render() {
+		var el = Faux.createElement('div')
+
+		el.setAttribute('class', 'my-svg-container')
+		el.setAttribute('id', 'grid')
+		var gridData = this.getGridData(); 
+		var grid = d3.select(el)
+			.append("svg")
+			.attr("class","my-svg")
+			.attr("preserveAspectRatio","xMidYMin slice")
+			.attr("viewBox","0 0 400 400")
+			.append("g")
+			.attr("transform","translate(0, 0)");
+		var row = grid.selectAll(".row")
+			.data(gridData)
+			.enter().append("g")
+			.attr("class", "row")
+		// row.each
+		// .each(function(d) {
+		// 	console.log('as43y 3', d)
+		// });
+			// .data(function(d) {
+			// 	return d; })
+			// .append("text")
+			// .attr("x", function(d) { return d.x; })
+			// .attr("y", function(d) { return d.y; })
+			// .text(function(d) { return d["data-name"]; })
+			// .text(function(d) {
+ // return d["data-name"]; })
+		// .each(function(d) {
+			// console.log('as43y 3', d)
+		// });
+
+		// var text = row.selectAll("text")
+		// // 	.data(gridData)
+		// 	.enter()
+		// 	.append("text");
+		var column = row.selectAll(".square")
+			.data(function(d) {
+				return d; })
+			.enter().append("rect")
+			.attr("class","square")
+			.attr("x", function(d) { return d.x; })
+			.attr("y", function(d) { return d.y; })
+			.attr("width", function(d) { return d.width; })
+			.attr("height", function(d) { return d.height; })
+			.style("fill", "#fff")
+			.style("stroke", "#222");
+
+		return el.toReact()
+	}
+};
+LineChart.propTypes = {
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
+    chartId: React.PropTypes.string
+};
+LineChart.defaultProps = {
+	width: 250,
+	height: 250,
+	chartId: 'v1_chart'
+};
 
 export const GridWrapper = (props) => {
 	const requiredXYs = <ul>
@@ -14,98 +124,6 @@ export const GridWrapper = (props) => {
 						y: {props.gridParams.requiredXYs[key].y}</li>
 				)}</ul>;
 
-	let LineChart = React.createClass({
-
-		propTypes: {
-			width:React.PropTypes.number,
-			height:React.PropTypes.number,
-			chartId:React.PropTypes.string
-		},
-
-		getDefaultProps: function() {
-			return {
-				width: 400,
-				height: 300,
-				chartId: 'v1_chart'
-			};
-		},
-		getInitialState:function(){
-			return {
-				width:this.props.width
-			};
-		},
-		render: function(){
-			let margin = {top: 5, right: 50, bottom: 20, left: 50},
-				w = this.state.width - (margin.left + margin.right),
-				h = this.props.height - (margin.top + margin.bottom);
-
-			let generalGridData = [];
-			let fieldSize = 40,
-				boardDimension = 10,
-				boardSize = boardDimension*fieldSize;
-    
-			for(let i = 0; i < boardDimension*boardDimension; i++) {
-				generalGridData.push({
-					x: i % boardDimension,
-					y: Math.floor(i / boardDimension),
-				});
-			};
-			let width = 400,
-				height = 300,
-				margins = {
-					top: 20,
-					left: 50,
-					bottom: 30,
-					right: 50,
-				},
-				gridAxisClassName = "grid-class",
-				x = function(d) {
-					return d.x;
-				},
-				xDomain = d3.extent(generalGridData, x),
-				xRange = [20, width - margins.left - margins.right],
-				xScale = 'linear',
-				y = function(d) {
-					return d.y;
-				},
-				yScale = 'linear',
-				yDomain = d3.extent(generalGridData, y),
-				yRange = [20, (height - margins.top - margins.bottom)];
-
-			let transform='translate(' + margins.left + ',' + margins.top + ')';
-
-			return (
-					<svg id={this.props.chartId} width={this.state.width} height={this.props.height}>
-						<g transform={transform}>
-							<Xgrid
-								width={width}
-								height={height}
-								margins={margins}
-								type={'x'}
-								gridAxisClassName={gridAxisClassName}
-								x={x}
-								xDomain={xDomain}
-								xRange={xRange}
-								xScale={xScale}
-								yScale={yScale}
-							/>
-							<Ygrid
-								width={width}
-								height={height}
-								margins={margins}
-								type={'y'}
-								gridAxisClassName= {gridAxisClassName}
-								y={y}
-								yDomain={yDomain}
-								yRange={yRange}
-								yScale={yScale}
-								xScale={xScale}
-							/>
-					  </g>
-					</svg>
-			);
-		}
-	});
 
 	return (
 		<div className={classes['grid-container']}>
